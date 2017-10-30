f3335534b4c7022638ce02d232fec57e0b04de8f
mimiqkz <npq1@hi.is>
2017-10-30 10:40:24 +0000

    sprite class

--- src/js/sprite.js
+++ src/js/sprite.js
@@ -1,47 +1,47 @@
 "use strict";
+class Sprite {
+  constructor(image, x, y, rotation) {
+    this.image = image
+    this.x = x
+    this.y = y
+    this.rotation
 
-function Sprite(image) {
-  this.image = image;
+    this.width = image.width;
+    this.height = image.height;
+    this.scale = 1;
+  }
+  drawAt() {
+    ctx.drawImage(this.image,x, y);
+  }
+  drawCentredAt() {
+    if (rotation === undefined) rotation = 0;
 
-  this.width = image.width;
-  this.height = image.height;
-  this.scale = 1;
+    let w = this.width,
+      h = this.height;
+
+    ctx.save();
+    ctx.translate(cx, cy);
+    ctx.rotate(rotation);
+    ctx.scale(this.scale, this.scale);
+
+    ctx.drawImage(this.image,
+      -w / 2, -h / 2);
+
+    ctx.restore();
+  }
+  drawWrappedCentredAt() {
+    let sw = g_canvas.width;
+
+    this.drawWrappedVerticalCentredAt(ctx, cx, cy, rotation);
+    this.drawWrappedVerticalCentredAt(ctx, cx - sw, cy, rotation);
+    this.drawWrappedVerticalCentredAt(ctx, cx + sw, cy, rotation);
+  }
+
+  drawWrappedVerticalCentredAt() {
+    let sh = g_canvas.height;
+
+    this.drawCentredAt(ctx, cx, cy, rotation);
+    this.drawCentredAt(ctx, cx, cy - sh, rotation);
+    this.drawCentredAt(ctx, cx, cy + sh, rotation);
+  }
 }
-
-Sprite.prototype.drawAt = function (ctx, x, y) {
-  ctx.drawImage(this.image,
-    x, y);
-};
-
-Sprite.prototype.drawCentredAt = function (ctx, cx, cy, rotation) {
-  if (rotation === undefined) rotation = 0;
-
-  let w = this.width,
-    h = this.height;
-
-  ctx.save();
-  ctx.translate(cx, cy);
-  ctx.rotate(rotation);
-  ctx.scale(this.scale, this.scale);
-
-  ctx.drawImage(this.image,
-    -w / 2, -h / 2);
-
-  ctx.restore();
-};
-
-Sprite.prototype.drawWrappedCentredAt = function (ctx, cx, cy, rotation) {
-  let sw = g_canvas.width;
-
-  this.drawWrappedVerticalCentredAt(ctx, cx, cy, rotation);
-  this.drawWrappedVerticalCentredAt(ctx, cx - sw, cy, rotation);
-  this.drawWrappedVerticalCentredAt(ctx, cx + sw, cy, rotation);
-};
-
-Sprite.prototype.drawWrappedVerticalCentredAt = function (ctx, cx, cy, rotation) {
-  let sh = g_canvas.height;
-
-  this.drawCentredAt(ctx, cx, cy, rotation);
-  this.drawCentredAt(ctx, cx, cy - sh, rotation);
-  this.drawCentredAt(ctx, cx, cy + sh, rotation);
-};
