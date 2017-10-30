768530b45269f72fa480250a0a8205e219ba3db7
mimiqkz <npq1@hi.is>
2017-10-30 09:54:50 +0000

    update on the control class

--- src/js/controls.js
+++ src/js/controls.js
@@ -8,36 +8,89 @@ class Controls {
     window.addEventListener("keydown", this.handleKeydown.bind(this));
     window.addEventListener("keyup", this.handleKeyup.bind(this));
   }
+  checkKey(e) {
+    switch (KEYS[e.keyCode]) {
+      case "q":
+        stopKey = true
+        break;
+      case "t":
+        doTimerShow = !doTimerShow
+        break;
+      case "p":
+        isUpdatePaused = !isUpdatePaused
+        break;
+      case "o":
+        return !step
+        break;
+      case "c":
+        doClear = !doClear
+        break;
+      case "b":
+        doBox = !doBox
+        break;
+      case "u":
+        undoBox = !undoBox
+        break;
+      case "f":
+        doFlipFlop = !doFlipFlop
+        break;
+      case "r":
+        doRender = !doRender
+        break;
+      default: break
+    }
+  }
+
+  handleKeydown(e) {
+    //Checking which key to use
+    this.checkKey(e)
+    // Remember that this key is down.
+    if (e.keyCode in KEYS) {
+        const keyName = KEYS[e.keyCode]
+        keys[keyName] = true
+    }
+    keys[e.keyCode] = true;
+  }
+
+  handleKeyup(e) {
+    // Remember that this key is up.
+    if (e.keyCode in KEYS) {
+        const keyName = KEYS[e.keyCode]
+        keys[keyName] = false
+    }
+    keys[e.keyCode] = false;
+  }
+
   initializeFullscreenListener() {
     $("#fullscreen").on("click", () => {
         this.toggleFullscreen();
     });
   }
   toggleFullscreen() {
-        if (document.fullscreenElement || document.webkitFullscreenElement ||
-            document.mozFullScreenElement || document.msFullscreenElement) {
+    if (document.fullscreenElement || document.webkitFullscreenElement ||
+        document.mozFullScreenElement || document.msFullscreenElement) {
 
-            if (document.exitFullscreen) {
-                document.exitFullscreen();
-            } else if (document.mozCancelFullScreen) {
-                document.mozCancelFullScreen();
-            } else if (document.webkitExitFullscreen) {
-                document.webkitExitFullscreen();
-            } else if (document.msExitFullscreen) {
-                document.msExitFullscreen();
-            }
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
 
-        } else {
-            const element = $("#map").get(0);
-            if (element.requestFullscreen) {
-                element.requestFullscreen();
-            } else if (element.mozRequestFullScreen) {
-                element.mozRequestFullScreen();
-            } else if (element.webkitRequestFullscreen) {
-                element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-            } else if (element.msRequestFullscreen) {
-                element.msRequestFullscreen();
-            }
+    } else {
+        const element = $("#map").get(0);
+        if (element.requestFullscreen) {
+            element.requestFullscreen();
+        } else if (element.mozRequestFullScreen) {
+            element.mozRequestFullScreen();
+        } else if (element.webkitRequestFullscreen) {
+            element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+        } else if (element.msRequestFullscreen) {
+            element.msRequestFullscreen();
         }
     }
+  }
 }
