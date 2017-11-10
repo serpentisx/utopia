3eae776df59d31669075bd89a4df27a15b20dca7
serpentisx <huy_van1@live.com>
2017-11-10 11:07:18 +0000

    Changed the collision mechanism

--- src/js/model/knight.js
+++ src/js/model/knight.js
@@ -13,16 +13,11 @@ class Knight extends Entity {
     this.velY = 0;
     this.gravity = 0.32;
 
-    this.dirX = 0;
-    this.dirY = 0;
-    this.collidedCells = [];
-
-    this.GO_LEFT = KEY_A
-    this.GO_RIGHT = KEY_D
-    this.JUMP = KEY_W
+    this.GO_LEFT = KEY_A;
+    this.GO_RIGHT = KEY_D;
+    this.JUMP = KEY_W;
 
     this.isJumping = false;
-    this.something = [];
   }
 
   setMap(map) {
@@ -34,7 +29,6 @@ class Knight extends Entity {
       avgVelY = (this.velY + finalVelY) / 2,
       dPosY = avgVelY * du;
 
-    this.dirY = dPosY;
     this.y += dPosY;
     this.velY = finalVelY;
   }
@@ -43,94 +37,102 @@ class Knight extends Entity {
     return (this.sprite.width / 2) * 0.9;
   }
 
-  //Could put this in entities
-  handleCollision(du) {
-    let row, col;
-    // Get coordinates
-    let left = this.x - this.sprite.width / 2,
-      right = this.x + this.sprite.width / 2,
-      top = this.y - this.sprite.height / 2,
-      bottom = this.y + this.sprite.height / 2;
-
-    // Debugging purposes
-    this.something[0] = [this.map.getCol(left), this.map.getRow(top)];
-    this.something[1] = [this.map.getCol(left), this.map.getRow(bottom)];
-    this.something[2] = [this.map.getCol(right), this.map.getRow(top)];
-    this.something[3] = [this.map.getCol(right), this.map.getRow(bottom)];
-    this.something[4] = [this.map.getCol(left), this.map.getRow(this.y)];
-    this.something[5] = [this.map.getCol(right), this.map.getRow(this.y)];
-    this.something[6] = [this.map.getCol(this.x), this.map.getRow(top)];
-    this.something[7] = [this.map.getCol(this.x), this.map.getRow(bottom)];
-
-
-    /***** VERTICAL MOVEMENT ******/
-
-    // CHECK BOTTOM-LEFT OR BOTTOM-RIGHT
-    if (this.dirY >= 0) {
-      if (this.map.isSolidTileAtXY(left, bottom) || this.map.isSolidTileAtXY(right, bottom)) {
-        row = this.map.getRow(bottom);
-        this.y = -this.sprite.height / 2 + this.map.getY(row);
-        this.velY = 0;
-      }
-    } else if (this.dirY < 0) {
-      // CHECK TOP-LEFT OR TOP-RIGHT
-      if (this.map.isSolidTileAtXY(left, top) || this.map.isSolidTileAtXY(right, top)) {
-        //change velocity so he doesnt keep jumping, looks like he is frozen
-        this.velY += 4;
-        row = this.map.getRow(top);
-        this.y = this.sprite.height / 2 + this.map.getY(row + 1);
-      }
-    }
-
-    /***** HORIZONTAL MOVEMENT******/
-    if (this.dirX > 0) {
-      // CHECK RIGHT-MIDDLE OR TOP RIGHT
-      if (this.map.isSolidTileAtXY(right, this.y) || this.map.isSolidTileAtXY(right, top)) {
-        col = this.map.getCol(right);
-        // the minus one below is so that once it resets the player
-        // it isnt still colliding
-        this.x = -this.sprite.width / 2 + this.map.getX(col) - 1;
-      }
-    } else if (this.dirX < 0) {
-      // CHECK LEFT-MIDDLE OR TOP LEFT
-      if (this.map.isSolidTileAtXY(left, this.y) || this.map.isSolidTileAtXY(left, top)) {
-        col = this.map.getCol(left);
-        this.x = this.sprite.width / 2 + this.map.getX(col + 1);
-      }
+  drawCollisions(collisions) {
+    for (var i in collisions) {
+      this.map.drawTile(collisions[i].x, collisions[i].y);
     }
   }
 
-  update(du) {
-    var worldWidth = this.map.width;
-    var worldHeight = this.map.height;
-    if (keys[this.GO_LEFT] && (this.x > this.sprite.width / 2)) {
-      this.x -= this.velX * du;
-      this.dirX = -this.velX;
-    } else if (keys[this.GO_RIGHT] && (this.x < worldWidth - this.sprite.width / 2)) {
-      this.x += this.velX * du;
-      this.dirX = this.velX;
-    } else this.dirX = 0;
+  handleCollisions(du) {
 
-    //Always update gravity
+    let halfWidth = this.sprite.width / 2;
+    let halfHeight = this.sprite.height / 2;
     this.applyGravity(du);
 
-    this.handleCollision(du);
+    let collisions = this.detectCollisionsWithPlatform();
+    this.collisions = collisions;
 
-    // Jump
+    let offSet = 3;
+
+    if (collisions["left"]) {
+      this.x = halfWidth + collisions["left"].x + collisions["left"].w + offSet;
+    }
+    if (collisions["right"]) {
+      this.x = collisions["right"].x - halfWidth - offSet;
+    }
+    if (collisions["top"]) {
+      this.velY = 0.01;
+      this.y = halfHeight + collisions["top"].y + collisions["top"].h;
+    }
+    if (collisions["bottom"]) {
+      // hard coded
+      if (collisions["right"] && Math.abs(collisions["bottom"].x - this.x + halfWidth) == 83) return;
+
+      this.velY = 0;
+      this.y = collisions["bottom"].y - halfHeight + 1;
+    }
+  }
+
+  detectCollisionsWithPlatform() {
+    let w = this.sprite.width / 2;
+    let tiles = this.map.getRectTiles(this.x, this.y);
+    let collisions = [];
+
+    tiles.forEach(tile => {
+      let collision = this.collidesWithPlatform(tile);
+      if (collision && this.map.isSolidTileAtXY(tile.x, tile.y)) {
+        collisions[collision] = tile;
+      }
+    });
+    return collisions;
+  }
+
+  // Two rectangles collision check
+  collidesWithPlatform(p) {
+    var x = this.x - this.sprite.width / 2;
+    var y = this.y - this.sprite.height / 2;
+    var w = this.sprite.width;
+    var h = this.sprite.height;
+    var dx = (x + w / 2) - (p.x + p.w / 2);
+    var dy = (y + h / 2) - (p.y + p.h / 2);
+    var width = (w + p.w) / 2;
+    var height = (h + p.h) / 2;
+    var crossWidth = width * dy;
+    var crossHeight = height * dx;
+    var collision = null;
+    //
+    if (Math.abs(dx) <= width && Math.abs(dy) <= height) {
+      if (crossWidth > crossHeight) {
+        collision = (crossWidth > (-crossHeight)) ? 'top' : 'right';
+      } else {
+        collision = (crossWidth > -(crossHeight)) ? 'left' : 'bottom';
+      }
+    }
+    return collision;
+  }
+
+  handleBoundary() {
+    let halfWidth = this.sprite.width / 2;
+    let halfHeight = this.sprite.height / 2;
+
+    if (this.y - halfHeight < 0) this.y = halfHeight;
+    if (this.y + halfHeight > this.map.height) this.y = this.map.height - halfHeight;
+    if (this.x - halfWidth < 0) this.x = halfWidth;
+    if (this.x + halfWidth > this.map.width) this.x = this.map.width - halfWidth;
+  }
+
+  update(du) {
+
+    if (keys[this.GO_LEFT])  this.x -= this.velX * du;
+    if (keys[this.GO_RIGHT]) this.x += this.velX * du;
+
     if (keys[this.JUMP] && !this.isJumping) {
+      this.velY = -11;
       this.isJumping = true;
-      this.velY = -10;
     }
 
-    // don't let player leaves the world's boundary
-
-    if (this.y - this.sprite.height / 2 < 0) {
-      this.y = this.sprite.height / 2;
-    }
-
-    if (this.y + this.sprite.height / 2 > worldHeight) {
-      this.y = worldHeight - this.sprite.height / 2;
-    }
+    this.handleCollisions(du);
+    this.handleBoundary();
 
     if (this.velY === 0) {
       this.isJumping = false; // might want to change this later
@@ -138,11 +140,8 @@ class Knight extends Entity {
   }
 
   render(ctx, xView, yView) {
-    for (var i = 0; i < this.something.length; i++) {
-      this.map.drawTile(this.something[i][0], this.something[i][1]);
-    }
+    this.drawCollisions(this.collisions);
     this.sprite.drawAtCenter(ctx, this.x - xView, this.y - yView);
     this.sprite.drawBoundary(ctx, this.x - xView, this.y - yView);
-    //    ctx.fillRect(this.x, this.y, 50, 50);
   }
 }
