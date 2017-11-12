bc32830b7915b3e3210fe587cd357c69bd5f43a8
meatyminx <flexayoung@gmail.com>
2017-11-12 21:21:59 +0100

    Map-edits
    Changed the map a bit, added an image for the ‘dungeon’
    
    Added an extra layer for the grid to render in the background but
    havent used it yet

--- src/js/Constants.js
+++ src/js/Constants.js
@@ -21,6 +21,9 @@ const KEY_3 = '3'.charCodeAt(0)
 const KEY_4 = '4'.charCodeAt(0)
 const KEY_5 = '5'.charCodeAt(0)
 const KEY_6 = '6'.charCodeAt(0)
+const KEY_7 = '7'.charCodeAt(0)
+const KEY_8 = '8'.charCodeAt(0)
+const KEY_9 = '9'.charCodeAt(0)
 
 //For updating
 const NOMINAL_UPDATE_INTERVAL = 16
