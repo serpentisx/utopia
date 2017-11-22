ab0754e44fd7f811f64a19b58af6793258d01dd7
serpentisx <huy_van1@live.com>
2017-11-22 13:39:37 +0000

    Fixed jump hovering at top edge

--- src/js/model/Entity.js
+++ src/js/model/Entity.js
@@ -67,6 +67,10 @@ class Entity {
     const halfWidth = this.sprite.width / 2;
     const halfHeight = this.sprite.height / 2;
 
+    if (this.y - halfHeight < 0) {
+      this.y = halfHeight;
+      this.velY = 0.01;
+    }
     if (this.y + halfHeight > this.map.height) this.y = this.map.height - halfHeight;
     if (this.x - halfWidth < 0) this.x = halfWidth;
     if (this.x + halfWidth > this.map.width) this.x = this.map.width - halfWidth;
