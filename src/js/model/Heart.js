309b17d2658df13d2a00dc1c4892f7981a4d2586
serpentisx <huy_van1@live.com>
2018-05-31 20:17:27 +0000

    Adjust heart rendering to full screen mode

--- src/js/model/Heart.js
+++ src/js/model/Heart.js
@@ -3,8 +3,7 @@ class Heart {
   constructor(lifePoints) {
     this.sprite = {};
     this.lifePoints = lifePoints;
-
-    this.startPosX = canvas.width-100;
+    
     this.posY = 50;
     this.spritesLoaded = false;
     this.loadSprites();
@@ -28,7 +27,7 @@ class Heart {
   render(ctx) {
     if (this.spritesLoaded) {
       for(var i = 0; i < this.lifePoints; i++) {
-        this.sprite.heart.drawAtCenter(ctx, this.startPosX - (i*50), this.posY);
+        this.sprite.heart.drawAtCenter(ctx, window.innerWidth - 100 - (i*50), this.posY);
       }
     }
   }
